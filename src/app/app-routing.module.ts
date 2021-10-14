1ce373f561f2b840dc077c0a4d8bfe69d64246ff
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-10-14 10:25:15 -0300

    Adiciona rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import {CadastrarComponent} from './components/cadastrar/cadastrar.component';
+import { HomeComponent } from './components/home/home.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'cadastrar',
+    component: CadastrarComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
